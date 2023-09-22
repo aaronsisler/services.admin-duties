6376c684217c990ac24abdfd596c5ed4dda5acff
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-09-21 21:29:58 -0400

    Updating the first client get test to be fully flushed out

---
+++ app/src/test/groovy/com/ebsolutions/config/TestConstants.java
@@ -0,0 +1,11 @@
+package com.ebsolutions.config;
+
+import java.time.LocalDateTime;
+
+public class TestConstants {
+    public static String adminServiceUrl = "http://localhost:8080";
+    public static String clientId = "mock-client-id";
+
+    public static LocalDateTime createdOn = LocalDateTime.of(2023, 7, 04, 1, 2, 34);
+    public static LocalDateTime lastUpdatedOn = LocalDateTime.of(2023, 7, 04, 12, 34, 56);
+}
--- app/src/test/groovy/com/ebsolutions/data/ClientSpec.groovy
+++ app/src/test/groovy/com/ebsolutions/data/ClientSpec.groovy
@@ -1,5 +1,6 @@
 package com.ebsolutions.data
 
+import com.ebsolutions.config.TestConstants
 import com.ebsolutions.models.Client
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.client.HttpClient
@@ -8,19 +9,20 @@ import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
 import spock.lang.Specification
 
+import java.time.temporal.ChronoUnit
+
 @MicronautTest
 class ClientSpec extends Specification {
     @Inject
     private HttpClient client;
 
-    private String clientsUrl = "http://localhost:8080/data/clients";
-    private String clientId = "body-kinect-wellness";
+    private String clientsUrl = TestConstants.adminServiceUrl + "/data/clients";
 
     def "Get a Client"() {
         given: "A client exists in the database"
-            // Need to seed data here
+            // Data seeded from Database init scripts
         when: "a request is made to the client endpoint"
-            HttpResponse<Client> response = client.toBlocking().exchange(clientsUrl + "/" + clientId, Client);
+            HttpResponse<Client> response = client.toBlocking().exchange(clientsUrl + "/" + TestConstants.clientId, Client);
 
         then: "the correct status code is returned"
             Assertions.assertEquals(java.net.HttpURLConnection.HTTP_OK, response.code());
@@ -28,7 +30,9 @@ class ClientSpec extends Specification {
         and: "the correct body is returned"
             Client client = response.body();
 
-            Assertions.assertEquals(clientId, client.getClientId());
-            Assertions.assertEquals("Body Kinect Wellness", client.getName());
+            Assertions.assertEquals(TestConstants.clientId, client.getClientId());
+            Assertions.assertEquals("Mock Client", client.getName());
+            Assertions.assertEquals(client.getCreatedOn().until(TestConstants.createdOn, ChronoUnit.SECONDS), 0);
+            Assertions.assertEquals(client.getLastUpdatedOn().until(TestConstants.lastUpdatedOn, ChronoUnit.SECONDS), 0);
     }
 }
