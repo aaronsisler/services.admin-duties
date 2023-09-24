fae1bf89484d3c509b8d458359bcb70eb6442570
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-09-24 14:44:24 -0400

    Standardizing all tests

--- app/src/test/groovy/com/ebsolutions/HealthyServiceSpec.groovy
+++ app/src/test/groovy/com/ebsolutions/HealthyServiceSpec.groovy
@@ -1,6 +1,6 @@
 package com.ebsolutions
 
-
+import com.ebsolutions.config.TestConstants
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.client.HttpClient
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
@@ -11,12 +11,15 @@ import spock.lang.Specification
 @MicronautTest
 class HealthyServiceSpec extends Specification {
     @Inject
-    private HttpClient client
+    private HttpClient httpClient
+
+    private String healthUrl = TestConstants.adminServiceUrl + "/health"
+
 
     def "Health endpoint is available"() {
         given: "The application is running"
         when: "a request is made to the health endpoint"
-            HttpResponse response = client.toBlocking().exchange("http://localhost:8080/health", String)
+            HttpResponse response = httpClient.toBlocking().exchange(healthUrl, String)
 
         then: "the correct status code is returned"
             Assertions.assertEquals(HttpURLConnection.HTTP_OK, response.code())
