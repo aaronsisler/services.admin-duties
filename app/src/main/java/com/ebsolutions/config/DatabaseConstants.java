2c7c6b07126de5d365d2e8df414ee443080949df
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-10-01 21:31:18 -0400

    Updating the events for October. Need to start on FTs for CSV now

--- app/src/main/java/com/ebsolutions/config/DatabaseConstants.java
+++ app/src/main/java/com/ebsolutions/config/DatabaseConstants.java
@@ -1,6 +1,6 @@
 package com.ebsolutions.config;
 
 public class DatabaseConstants {
-    //    public static String DATABASE_TABLE_NAME = "SERVICES_ADMIN_DUTIES_BETA";
-    public static String DATABASE_TABLE_NAME = "SERVICES_ADMIN_DUTIES_TESTS";
+    public static String DATABASE_TABLE_NAME = "SERVICES_ADMIN_DUTIES_BETA";
+//    public static String DATABASE_TABLE_NAME = "SERVICES_ADMIN_DUTIES_TESTS";
 }
